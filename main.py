c131a7d4b4510a0fae27ddd8608440412544b603
wangchao668 <apk66578@163.com>
2023-12-20 22:03:15 +0800

    first

---
+++ main.py
@@ -0,0 +1,144 @@
+import streamlit as st
+from spider import Spiders
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import re
+
+plt.rcParams['font.sans-serif']=['SimHei']
+
+# 设置页面
+st.set_page_config(page_title="房地产数据爬虫", layout="wide")
+
+# CSS样式
+st.markdown("""
+    <style>
+    .big-font {
+        font-size:30px !important;
+        font-weight: bold;
+    }
+    .custom-font {
+        font-size:18px;
+    }
+    .custom-button {
+        background-color: #4CAF50;
+        color: white;
+        padding: 0.25em 0.5em;
+        text-align: center;
+        display: inline-block;
+        font-size: 16px;
+        margin: 4px 2px;
+        cursor: pointer;
+        border-radius: 8px;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+
+# 界面标题
+st.markdown('<p class="big-font">房地产数据爬虫</p>', unsafe_allow_html=True)
+
+# 创建一个爬虫实例
+spider = Spiders(url="https://gz.fang.lianjia.com/")
+
+# 侧边栏配置
+with st.sidebar:
+    st.header('配置')
+    spider.get_cityinfo()
+    city = st.selectbox('选择城市', list(spider.city_dict.keys()))
+    max_pages = 200  # 默认的最大页数
+    if city:
+        spider.city_url = spider.city_dict[city]
+        spider.city_name = city
+        max_pages = spider.get_maxpagenum()  # 获取并显示选定城市的最大页数
+        if max_pages == 'error':
+            st.error('无法获取该城市的最大页数。')
+            max_pages = 200
+        else:
+            st.write(f'该城市共有 {max_pages} 页数据可供爬取。')
+
+    # 确定爬取页数
+    pages_choice = st.number_input('选择要爬取的页数', min_value=1,
+                                   max_value=max_pages if max_pages != 'error' else 100, step=1)
+
+# 主界面
+col1, col2 = st.columns(2)
+with col1:
+    st.markdown('<p class="custom-font">请选择城市和爬取页数，然后点击下方按钮开始爬取数据。</p>', unsafe_allow_html=True)
+
+import io  # 用于处理内存中的文件
+
+# ... [您的其他代码] ...
+
+# 在Streamlit中显示DataFrame和下载按钮
+if st.button('爬取数据', key="fetch_data"):
+    with st.spinner('正在爬取数据...'):
+        spider.get_citydata(pages_choice)
+        if spider.data_list:
+            st.success('数据爬取完成')
+            df = pd.DataFrame(spider.data_list)
+            st.dataframe(df)
+
+            # 创建三列布局
+            col1, col2, col3 = st.columns(3)
+
+            # CSV下载
+            with col1:
+                csv = df.to_csv(index=False)
+                st.download_button(label="下载数据为CSV", data=csv, file_name='data.csv', mime='text/csv')
+
+            # JSON下载
+            with col2:
+                # 确保在转换为JSON之前设置了 ensure_ascii=False
+                json_data = df.to_json(orient='records', force_ascii=False, lines=True)
+                st.download_button(
+                    label="下载数据为JSON",
+                    data=json_data.encode('utf-8'),  # 编码为utf-8
+                    file_name='data.json',
+                    mime='application/json'
+                )
+
+            # Excel下载
+            with col3:
+                to_write = io.BytesIO()
+                df.to_excel(to_write, index=False, engine='openpyxl')  # 写入到BytesIO对象
+                to_write.seek(0)  # 移动到文件开始位置
+                st.download_button(label="下载数据为Excel", data=to_write, file_name='data.xlsx',
+                                   mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+
+
+            # 数据清洗函数
+
+            # 清洗 '均价' 列的函数
+
+        else:
+            st.error('未能获取数据')
+    df = pd.DataFrame(spider.data_list)
+
+    # 清洗 '均价' 列的函数
+    def extract_number(text):
+        match = re.search(r'(\d+)', text)
+        return int(match.group()) if match else None
+
+    # 清洗数据
+    df['均价数值'] = df['均价'].apply(extract_number)
+
+    # 基本统计分析
+    average_price = df['均价数值'].mean()
+    median_price = df['均价数值'].median()
+    st.write(f'均价的平均值是：{average_price}元/㎡')
+    st.write(f'均价的中位数是：{median_price}元/㎡')
+
+    # 可视化 - 均价分布情况
+    fig, ax = plt.subplots()
+    sns.histplot(df['均价数值'], kde=False, ax=ax)
+    ax.set_title('均价分布情况')
+    ax.set_xlabel('价格 (元/㎡)')
+    ax.set_ylabel('频数')
+
+    # 使用列来居中显示图表
+    col1, col2, col3 = st.columns([1, 6, 1])  # 比例为 1:6:1
+    with col2:  # 使用中间的列来显示图表
+        st.pyplot(fig)
+
+
+
---
+++ spider.py
@@ -0,0 +1,98 @@
+import streamlit as st
+import random, requests
+from lxml import etree
+
+
+# 定义一个生成随机user-agent的类
+class FakeUA:
+    num1 = random.randint(55, 62)
+    num2 = random.randint(0, 3200)
+    num3 = random.randint(0, 140)
+    version_ = 'Chrome/{}.0.{}.{}'.format(num1, num2, num3)
+    os_type = ['(Windows NT 6.1; WOW64)', '(Windows NT 10.0; WOW64)', '(X11; Linux x86_64)',
+               '(Macintosh; Intel Mac OS X 10_12_6)']
+
+    @classmethod
+    def get_ua(cls):
+        return ''.join(
+            ['Mozilla/5.0', random.choice(cls.os_type), 'AppleWebKit/537.36', '(KHTML, like Gecko)', cls.version_,
+             'Safari/537.36'])
+
+# 定义爬虫类Spiders，继承FakeUA类
+class Spiders(FakeUA):
+    def __init__(self, url, headers=None):
+        self.url = url
+        self.headers = headers
+        if not self.headers:
+            self.headers = {}
+            self.headers['user-agent'] = self.get_ua()
+
+    def spider(self, url):
+        try:
+            res = requests.get(url, headers=self.headers)
+            if res.status_code == 200:
+                res.encoding = 'utf-8'
+                return res.text
+        except requests.ConnectionError:
+            st.error('无法获取数据，请检查网络连接或站点是否可用')
+            return None
+
+    def get_cityinfo(self):
+        res = self.spider(self.url)
+        if res:
+            html = etree.HTML(res)
+            cityenums = html.xpath('//div[@class="city-enum fl"]')
+            self.city_dict = {}
+            for i in cityenums:
+                city_names = i.xpath('./a/text()')
+                city_urls = i.xpath('./a/@href')
+                for k, v in zip(city_names, city_urls):
+                    self.city_dict[k.strip()] = 'https:' + v
+
+    def get_maxpagenum(self):
+        loupan_url = self.city_url + '/loupan'
+        res = self.spider(loupan_url)
+        if not res:
+            return 'error'
+        html = etree.HTML(res)
+        total_num = int(html.xpath('//span[@class="value "]/text()')[0])  # 注意value后面有空格
+        maxpage = total_num // 10 if total_num % 10 == 0 else total_num // 10 + 1
+        return maxpage
+
+    @st.cache(allow_output_mutation=True)
+    def get_citydata(self, pages_choice):
+        maxpage_num = self.get_maxpagenum()
+        if maxpage_num == 'error':
+            st.error('获取最大页数失败，无法继续。')
+            return 'error'
+        st.write(f'您选择获取数据的城市为{self.city_name}，共有{maxpage_num}页数据')
+        self.data_list = []
+
+        # 显示进度条
+        progress_bar = st.progress(0)
+
+        for i in range(1, pages_choice + 1):
+            # 更新进度条
+            progress_bar.progress(i / pages_choice)
+            st.write(f'正在获取第{i}页数据...')
+
+            city_page_url = f"{self.city_url}/loupan/pg{str(i)}"
+            res = self.spider(city_page_url)
+            if res:
+                html = etree.HTML(res)
+                map = lambda x: x[0] if x else '未知'
+                items = html.xpath('//div[@class="resblock-desc-wrapper"]')
+                self.fieldnames = ['名称', '地址', '户型', '面积', '均价', '总价']
+                for item in items:
+                    title = map(item.xpath('div[@class="resblock-name"]/a/text()'))
+                    address = item.xpath('string(div[@class="resblock-location"])').replace(' ', '').replace('\n', '')
+                    rooms = map(item.xpath('a[@class="resblock-room"]/span/text()'))
+                    area = map(item.xpath('div[@class="resblock-area"]/span/text()'))
+                    average_price = ''.join(item.xpath('div[@class="resblock-price"]//span//text()')).replace('\xa0', '')
+                    sum_price = map(item.xpath('div[@class="resblock-price"]/div[@class="second"]/text()'))
+                    if sum_price == []: sum_price = '价格待定'
+                    data_list = [title, address, rooms, area, average_price, sum_price]
+                    data_dict = {k: v for k, v in zip(self.fieldnames, data_list)}
+                    self.data_list.append(data_dict)
+                    st.write(f'名称：{title}    地址：{address}    户型：{rooms}    面积：{area}    均价：{average_price}    总价：{sum_price}')
+        st.write(f'\n本次共获取数据 {len(self.data_list)} 条')
